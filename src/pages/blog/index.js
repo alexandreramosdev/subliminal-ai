ef6c4cd67234d53d4f39cd5bdc2f21e4d2d7c18e
Alexandre Ramos dos Santos <alexandresantos84@gmail.com>
2019-12-13 12:50:57 -0300

    feat: init page blog

---
+++ src/pages/blog/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+import { Layout } from '../../components'
+// import { Container } from './styles';
+
+const blog = () => <Layout><div >! Developing BLOG </div></Layout>;
+
+export default blog;
