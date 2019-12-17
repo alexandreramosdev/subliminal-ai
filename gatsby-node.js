602d53f1a0722c206d980fb14fb95e6416e1797f
Alexandre Ramos dos Santos <alexandresantos84@gmail.com>
2019-12-17 03:16:30 -0300

    fix: build slider

--- gatsby-node.js
+++ gatsby-node.js
@@ -62,3 +62,18 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
     })
   }
 }
+
+exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+  if (stage === "build-html") {
+    actions.setWebpackConfig({
+      module: {
+        rules: [
+          {
+            test: /ventura-slider/,
+            use: loaders.null(),
+          },
+        ],
+      },
+    })
+  }
+}
