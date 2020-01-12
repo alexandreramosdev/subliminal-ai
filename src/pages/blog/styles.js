d0cb33d86140f1ae6e08b13707d442333c794cc1
Alexandre Ramos dos Santos <alexandresantos84@gmail.com>
2020-01-12 20:11:08 -0300

    styled: fix deploy

--- src/pages/blog/styles.js
+++ src/pages/blog/styles.js
@@ -16,47 +16,63 @@ export const Grid = styled.div`
   @media (min-width: 900px) {
       grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr;
 
-      & > ${Card}:nth-child(1) {
+      ${Card}:nth-child(1) {
        grid-column: span 4;
       }
 
-      & > ${Card}:nth-child(2){
+      ${Card}:nth-child(2){
        grid-column: span 2;
       }
 
-      & > ${Card}:nth-child(3),
-      & > ${Card}:nth-child(4){
+       ${Card}:nth-child(3){
+         grid-column: span 3;
+       }
+      ${Card}:nth-child(4){
        grid-column: span 3;
       }
     
-      & > ${Card}:nth-child(5){
+      ${Card}:nth-child(5){
         grid-column: span 4;
       }
 
-      & > ${Card}:nth-child(6){
+      ${Card}:nth-child(6){
         grid-column: span 2;
       }
 
-      & > ${Card}:nth-child(7), 
-      & > ${Card}:nth-child(8),
-      & > ${Card}:nth-child(9){
+      ${Card}:nth-child(7){
+        grid-column: span 2;
+      }
+      ${Card}:nth-child(8){
+        grid-column: span 2;
+      }
+      ${Card}:nth-child(9){
        grid-column: span 2;
       }
       
-      & > ${Card}:nth-child(10) {
+      ${Card}:nth-child(10) {
         grid-column: 1 / -1;
       }
 
-      & > ${Card}:nth-child(11), 
-      & > ${Card}:nth-child(12),
-      & > ${Card}:nth-child(13), 
-      & > ${Card}:nth-child(14){
+      ${Card}:nth-child(11){
+        grid-column: span 3;
+      } 
+      ${Card}:nth-child(12){
+        grid-column: span 3;
+      }
+      ${Card}:nth-child(13){
+        grid-column: span 3;
+      } 
+      ${Card}:nth-child(14){
        grid-column: span 3;
       }
 
-      & > ${Card}:nth-child(15), 
-      & > ${Card}:nth-child(16),
-      & > ${Card}:nth-child(17){
+      ${Card}:nth-child(15){
+        grid-column: span 2
+      }
+      ${Card}:nth-child(16){
+        grid-column: span 2
+      }
+      ${Card}:nth-child(17){
        grid-column: span 2
      }
      
