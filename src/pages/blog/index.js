ca19bf081cea32a6b6ccdc03c250a2e10f637f58
Alexandre Ramos dos Santos <alexandresantos84@gmail.com>
2020-01-12 22:39:35 -0300

    styled: fix deploy

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,7 +1,80 @@
 import React from "react"
+import styled from 'styled-components'
 
 import { Layout, CallTo, Trusted, Card } from "../../components"
-import { Grid } from './styles';
+
+import { Wrapper as WrapperCard } from '../../components/card/styles'
+
+export const Grid = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+
+  @media (min-width: 600px) {
+      grid-template-columns: 1fr 1fr;
+      width:100%;
+      max-width: 1320px;
+      margin: 0 auto;
+  }
+
+  @media (min-width: 900px) {
+      grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr;
+
+      & > ${WrapperCard}:nth-child(1) {
+       grid-column: span 4;
+      }
+
+      & > ${WrapperCard}:nth-child(2){
+       grid-column: span 2;
+      }
+
+       & > ${WrapperCard}:nth-child(3){
+         grid-column: span 3;
+       }
+      & > ${WrapperCard}:nth-child(4){
+       grid-column: span 3;
+      }    
+      & > ${WrapperCard}:nth-child(5){
+        grid-column: span 4;
+      }
+      & > ${WrapperCard}:nth-child(6){
+        grid-column: span 2;
+      }
+      & > ${WrapperCard}:nth-child(7){
+        grid-column: span 2;
+      }
+      & > ${WrapperCard}:nth-child(8){
+        grid-column: span 2;
+      }
+      & > ${WrapperCard}:nth-child(9){
+       grid-column: span 2;
+      }      
+      & > ${WrapperCard}:nth-child(10) {
+        grid-column: 1 / -1;
+      }
+      & > ${WrapperCard}:nth-child(11){
+        grid-column: span 3;
+      } 
+      & > ${WrapperCard}:nth-child(12){
+        grid-column: span 3;
+      }
+      & > ${WrapperCard}:nth-child(13){
+        grid-column: span 3;
+      } 
+      & > ${WrapperCard}:nth-child(14){
+       grid-column: span 3;
+      }
+
+      & > ${Card}:nth-child(15){
+        grid-column: span 2
+      }
+      & > ${Card}:nth-child(16){
+        grid-column: span 2
+      }
+      & > ${Card}:nth-child(17){
+       grid-column: span 2
+     }
+  }
+`;
 
 const blog = () => (
   <Layout>
