052aad9c5a9829ddf6c67c80a5fc48847387b5f3
Alexandre Ramos dos Santos <alexandresantos84@gmail.com>
2020-01-18 01:10:55 -0300

    refactor

--- src/components/news/index.js
+++ src/components/news/index.js
@@ -1,6 +1,6 @@
 import React, { useRef } from "react"
 import TinySlider from "tiny-slider-react"
-import { useStaticQuery } from "gatsby"
+import { useStaticQuery, graphql } from "gatsby"
 
 import Card from "../card"
 
