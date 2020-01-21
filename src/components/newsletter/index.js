a81d7527009e4d90221990c479c2e4d0933e0f84
Alexandre Ramos dos Santos <alexandresantos84@gmail.com>
2020-01-20 22:08:26 -0300

    feat: config submit netlify newsletter

--- src/components/newsletter/index.js
+++ src/components/newsletter/index.js
@@ -6,29 +6,22 @@ import { Section, Wrapper, Title, Subtitle, Send, Input, Form } from "./styles"
 import text from "../../data/text.json"
 
 const Newsletter = () => {
-  const [email, setEmail] = useState("")
-
-  const submit = e => {
-    e.preventDefault()
-    console.log(email)
-    setEmail("Sending ...")
-
-    setTimeout(() => setEmail(""), 1000)
-  }
-
   return (
     <Section>
       <Wrapper>
         <Title>{text.newsletter.title}</Title>
         <Subtitle>{text.newsletter.subtitle}</Subtitle>
-        <Form onSubmit={submit}>
+        <Form
+          name="newsletter"
+          method="POST"
+          data-netlify="true" >
+          <input type='hidden' name='form-name' value="newsletter" />
           <Input
             type="text"
             placeholder="Email Address..."
-            value={email}
-            onChange={e => setEmail(e.target.value)}
+            name="name"
           />
-          <Send>
+          <Send type='submit'>
             {text.newsletter.action} <img src={arrow} alt="..." />
           </Send>
         </Form>
